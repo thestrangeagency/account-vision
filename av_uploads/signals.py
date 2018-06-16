2b8b213c901b29b1e472a1623839c0283275fcf6
lucas <lucas@thestrangeagency.com>
2018-06-16 08:54:56 +0200

    only add activity for uploaded files, btw

--- av_uploads/signals.py
+++ av_uploads/signals.py
@@ -8,7 +8,8 @@ from av_uploads.utils import delete_s3_object
 
 @receiver(post_save, sender=S3File)
 def s3file_post_save(sender, instance, created, *args, **kwargs):
-    action.send(instance.user, verb='updated', target=instance)
+    if instance.uploaded:
+        action.send(instance.user, verb='updated', target=instance)
 
 
 @receiver(post_delete, sender=S3File)
