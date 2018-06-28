a7400bcbf1fa562b1dcd6c3cf2361efe5689cae8
lucas <lucas@thestrangeagency.com>
2018-06-28 16:56:12 +0200

    pre-populate contact form

--- av_contact/views.py
+++ av_contact/views.py
@@ -9,6 +9,13 @@ class ContactView(FormView):
     template_name = 'contact.html'
     form_class = ContactForm
     success_url = reverse_lazy('contact')
+    
+    def get_initial(self):
+        initial = super(ContactView, self).get_initial()
+        if not self.request.user.is_anonymous():
+            initial['name'] = self.request.user
+            initial['email'] = self.request.user.email
+        return initial
 
     def form_valid(self, form):
         form.save()
